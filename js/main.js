9df5990255ffcc12aa928dcf615ce055dba09f3f
jharteaga <jh.arteaga09@gmail.com>
2021-04-01 10:44:01 -0700

    Add options buttons functionality calling all, trending, and popular movies

--- js/main.js
+++ js/main.js
@@ -5,24 +5,28 @@ const API_KEY = '9dfd01779b6fdeb1cde19f1c010bb6a9';
 /**
  * TMDB Endpoints URLs
  */
+const ALL_MOVIES_URL = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
 const TRENDING_MOVIES_URL = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
+const POPULAR_MOVIES_URL = `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`;
 const GENRES_URL = `https://api.themoviedb.org/3/genre/movie/list?api_key=${API_KEY}&language=en-US`;
 const IMAGE_URL = `https://image.tmdb.org/t/p/w300`;
 /**
  * Functions
  */
-const showDetails = (element) => {
-	element.classList.toggle('fa-eye-slash');
-	element.classList.toggle('fa-eye');
-	element.classList.value.includes('fa-eye-slash')
-		? (element.style.color = '#03dac6')
-		: (element.style.color = '#f0f0f0');
-	const detailRow = element.parentElement.nextElementSibling;
-	detailRow.classList.toggle('hideDetail');
+const getAllMovies = async () => {
+	const response = await fetch(ALL_MOVIES_URL);
+	const { results } = await response.json();
+	return results;
 };
 
 const getDailyTrendingMovies = async () => {
 	const response = await fetch(TRENDING_MOVIES_URL);
+	const { results } = await response.json();
+	return results;
+};
+
+const getPopularMovies = async () => {
+	const response = await fetch(POPULAR_MOVIES_URL);
 	const { results } = await response.json();
 	return results;
 };
@@ -39,27 +43,46 @@ const getGenres = async () => {
 	const { genres } = await response.json();
 	return genres;
 };
+
+const showDetails = (element) => {
+	element.classList.toggle('fa-eye-slash');
+	element.classList.toggle('fa-eye');
+	element.classList.value.includes('fa-eye-slash')
+		? (element.style.color = '#03dac6')
+		: (element.style.color = '#f0f0f0');
+	const detailRow = element.parentElement.nextElementSibling;
+	detailRow.classList.toggle('hideDetail');
+};
+
+const changeSelectedOption = (optionsArr, optionIndex) => {
+	optionsArr.forEach((option, index) => {
+		if (index === optionIndex) {
+			option.classList.add('selected');
+		} else {
+			option.classList.remove('selected');
+		}
+	});
+};
 /**
  * Display Data Table
  */
-const displayDataTable = async () => {
+const displayDataTable = async (func) => {
 	tableContainer.innerHTML = '';
 
 	let output =
 		'<table id="dataTable"><thead><th>Title</th><th>Genre</th><th>Language</th><th>Rating</th><th>Details</th></thead><tbody>';
 
-	const moviesArr = await getDailyTrendingMovies();
+	const moviesArr = await func();
 
 	for (let item of moviesArr) {
 		const movie = await getMovie(item.id);
-		console.log(movie);
 		output += `<tr>
 	              <td>${movie.title}</td>
 	              <td>${movie.genres[0].name}</td>
 	              <td>${
 									movie.spoken_languages.length > 0
 										? movie.spoken_languages[0].english_name
-										: movie.original_language
+										: 'Unknown'
 								}</td>
 	              <td>${movie.vote_average}</td>
 	              <td><i class="far fa-eye-slash" onclick="showDetails(this)"></i></td>
@@ -77,4 +100,33 @@ const displayDataTable = async () => {
 	tableContainer.innerHTML = `${output}</tbody></table>`;
 };
 
-displayDataTable();
+const displayTrendingMovies = () => {
+	displayDataTable(getDailyTrendingMovies);
+};
+
+const displayAllMovies = () => {
+	displayDataTable(getAllMovies);
+};
+
+const displayPopularMovies = () => {
+	displayDataTable(getPopularMovies);
+};
+/**
+ * Event Listeners
+ */
+allMovies.addEventListener('click', (e) => {
+	changeSelectedOption([allMovies, trending, popular], 0);
+	displayAllMovies();
+});
+
+trending.addEventListener('click', () => {
+	changeSelectedOption([allMovies, trending, popular], 1);
+	displayTrendingMovies();
+});
+
+popular.addEventListener('click', () => {
+	changeSelectedOption([allMovies, trending, popular], 2);
+	displayPopularMovies();
+});
+
+displayAllMovies();
